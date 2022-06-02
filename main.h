2cc7da2c1ccc44450526c5a55d7b7e9a3fd82162
ORS <shubham.diwane@thundersoft.com>
2022-06-01 21:10:01 -0700

    project file

--- main.h
+++ main.h
@@ -5,6 +5,7 @@
 #include<pthread.h>
 #define MAX 30
 #define BEAST 200
+int main();
 void Training();// for training registration and feedback
 void Voting();// for voating request and responce
 void registration_fun();// for training registration
