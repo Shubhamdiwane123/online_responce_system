5fecc2778c434d495ce91f0495dd59b53a063c15
ORS <shubham.diwane@thundersoft.com>
2022-06-02 00:18:57 -0700

    project file

--- Voting_Responce.c
+++ Voting_Responce.c
@@ -1,110 +1,103 @@
 #include"Voting_Responce.h"
-char Fname[50]; // for scanning filename from user 
+char Fname[20],buf[80]; // for scanning filename from user 
 pthread_mutex_t lock;
 FILE *F; //To access file operations
 void *person1(void *p)
 {
-pthread_mutex_lock(&lock);
-	char data[20],ch;//(data=for taking data from user),(ch=taking data from file)
-	char buf[80],*r;//(buf=to storing file data into buffer),(r=taking data upto null using strtok and printing)
-	int i=0;//to store the data into buffer using this variable
-	F=fopen(Fname,"r");
-	if(F==NULL)
+	pthread_mutex_lock(&lock);
+	char str[20];
+	int i;
+	char ch='\n';
+	F=fopen(Fname,"a");
+	fputc(ch,F);
+	for(i=0;buf[i];i++)
 	{
-		printf("file is not present\n");
-	return 0;
-	}
-	else
-	{
-//	F=fopen(Fname,"r");
-	while(((ch=fgetc(F))!=EOF))//taking loop and copying each data into buffer 
-		buf[i++]=ch;
-	buf[i]='\0';
-	fclose(F);
-	ch='\t'; 
-	F=fopen(Fname,"a");//opening file to append data in next line
-	r=strtok(buf,",");
-	while(r!=NULL)
-	{
+		if((buf[i])!='\t')
+			printf("%c",buf[i]);
+		else
+		{
+			printf("\n");
+			scanf("%s",str);
+			fprintf(F,"%s\t",str);
+		}
 
-		printf("%s\n",r);
-		//r=strtok(NULL,",");
-		scanf("%s",data);//taking data from user
-		fputs(data,F);//putting the data into file
-		fputc(ch,F);//for going to next coloumn
-		r=strtok(NULL,",");
 	}
 	fclose(F);
 	pthread_mutex_unlock(&lock);
-	}
 }
-
 void *person2(void *p)
 {
 	pthread_mutex_lock(&lock);
-	char data[20],ch,buf[80],*r;
-	int i=0;
-	F=fopen(Fname,"r");
-	while(((ch=fgetc(F))!=EOF))
-		buf[i++]=ch;
-	buf[i]='\0';
-	fclose(F);
-	puts(buf);
-	ch='\t';
-	char c='\n';
+	char str[20];
+	int i;
+	char ch='\n';
 	F=fopen(Fname,"a");
-	fputc(c,F);
-	r=strtok(buf,",");
-	while(r!=NULL)
+	fputc(ch,F);
+	for(i=0;buf[i];i++)
 	{
-		printf("%s\n",r);
-		r=strtok(NULL,",");
-		scanf("%s",data);
-		fputs(data,F);
-		fputc(ch,F);
+		if((buf[i])!='\t')
+			printf("%c",buf[i]);
+		else
+		{
+			printf("\n");
+			scanf("%s",str);
+			fprintf(F,"%s\t",str);
+		}
+
 	}
 	fclose(F);
 	pthread_mutex_unlock(&lock);
 }
+
 void *person3(void *p)
 {
-
 	pthread_mutex_lock(&lock);
-	char data[20],ch,buf[80],*r;
-	int i=0;
-	F=fopen(Fname,"r");
-	while(((ch=fgetc(F))!=EOF))
-		buf[i++]=ch;
-	buf[i]='\0';
-	fclose(F);
-	puts(buf);
-	ch='\t';
-	char c='\n';
+	char str[20];
+	int i;
+	char ch='\n';
 	F=fopen(Fname,"a");
-        fputc(c,F);
-	r=strtok(buf,",");
-	i=0;
-	while(r!=NULL)
+	fputc(ch,F);
+	for(i=0;buf[i];i++)
 	{
-		printf("%s\n",r);
-		r=strtok(NULL,",");
-		scanf("%s",data);
-		fputs(data,F);
-		fputc(ch,F);
+		if((buf[i])!='\t')
+			printf("%c",buf[i]);
+		else
+		{
+			printf("\n");
+			scanf("%s",str);
+			fprintf(F,"%s\t",str);
+		}
+
 	}
 	fclose(F);
 	pthread_mutex_unlock(&lock);
 }
 void Voting_Responce()
 {
+	char ch;
+	int i=0;
 	system("ls");
 	printf("enter file name\n");
 	scanf("%s",Fname);
-	pthread_t t1,t2,t3;
-	pthread_create(&t1,NULL,person1,0);
-	pthread_create(&t2,NULL,person2,0);
-	pthread_create(&t3,NULL,person3,0);
-	pthread_join(t1,NULL);
-	pthread_join(t2,NULL);
-	pthread_join(t3,NULL);
+	F=fopen(Fname,"r");
+	if(F==NULL)
+	{
+		printf("file not present\n");
+		return ;
+	}
+	else
+	{
+		while(((ch=fgetc(F))!=EOF))
+			buf[i++]=ch;
+		buf[i++]=',';
+		buf[i]='\0';
+		fclose(F);
+	}
+pthread_t t1,t2,t3;
+pthread_create(&t1,NULL,person1,0);
+pthread_create(&t2,NULL,person2,0);
+pthread_create(&t3,NULL,person3,0);
+pthread_join(t1,NULL);
+pthread_join(t2,NULL);
+pthread_join(t3,NULL);
 }
