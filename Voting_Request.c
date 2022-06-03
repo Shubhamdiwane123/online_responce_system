4fafac5e1fde3e7659f53fbe063c4c432c53bdfc
ORS <shubham.diwane@thundersoft.com>
2022-06-03 01:03:50 -0700

    project file

--- Voting_Request.c
+++ Voting_Request.c
@@ -1,4 +1,6 @@
+#include "main.h"
 #include "Voting_Request.h"
+
 void Voting_Request()
 {
 	char n;
@@ -18,15 +20,9 @@ void Voting_Request()
 		               break;
 		    case '2' : fptr[1]();
 		               break;
-		   case '*' : main();
-			      break;
-		   
+		    case '*' : main();
+		               break;
 		    default  : invalid_option++;
-			       if(invalid_option==3)
-			       {
-				       printf("you chose 3 times wrong option that's why is go to main mainu\n");
-				       main();
-			       }
 			           printf("Invalid choice.\n");
 		               break;
 	        }	
@@ -34,7 +30,7 @@ void Voting_Request()
 	    else
 	    {
 	    	printf("\nError: User entered Invalid Option for three times\n\n");
-	//		main();
+			main();
 	    }
     }
 }
@@ -51,75 +47,90 @@ void CreateVotingRequest()
 			printf("Enter the excel file name : \n");
 			scanf(" %[^\n]",a);
 		}while(a[0] == '_' || a[0] >= '0' && a[0] <= '9');
-		for(i=0; a[i]; i++) // This for loop is to check the criteria for creating filename
-		{
-			if(a[i] >= '0' && a[i] <= '9' && a[i+1]>='a' && a[i+1] <= 'z')
+	        for(i=0; a[i]; i++) // This for loop is to check the criteria for creating filename
+	        {
+			if(a[i] >= '0' && a[i] <= '9' && a[i+1] >= 'a' && a[i+1] <= 'z')
 			{
 				printf("Wrong format\n");
 				goto scan;
 			}
-			if(a[i] >= '0' && a[i] <= '9' && a[i+1]>='A' && a[i+1] <= 'Z')
+			if(a[i] >= '0' && a[i] <= '9' && a[i+1] >= 'A' && a[i+1] <= 'Z')
 			{
 				printf("Wrong format\n");
 				goto scan;
 			}
-			if(a[i] >= '!' && a[i] <= '/' || a[i] <= '@' || a[i] >= '[' && a[i] <= '`' || a[i] >= '{' && a[i] <= '~')
+			if(a[i] >= '!' && a[i] <= '/' || a[i] >= ':' && a[i] <= '@' || a[i] >= '[' && a[i] <= '`' || a[i] >= '{' && a[i] <= '~')
 			{
-				printf("Wrong format\n");
-				goto scan;
+			printf("Wrong format\n");
+			goto scan;
 			}
 		}
 		strcpy(c,a);
-		for(i=0;a[i];i++) // loop for adding '_' in place of space
+		for(i=0; a[i]; i++)
 		{
-			if(a[i] == ' ')
-			   a[i] = '_';
-		}
-		strcat(a,b);
-		//printf("%s\n",a);
-		FILE*fp;
-		fp = fopen(a,"w");
-		int n;
-		printf("Enter the fields for %s Request form\n", c);
-		do
-		{
-			char read[MAX];
-			scanf(" %[^\n]", read);
-            fprintf(fp,"%s\t", read);
-            i++;
-            printf("Do you want to add another field (y/n)\n");
-            scanf(" %c",&field);
-		}while(field == 'y' || field == 'Y');
-	fclose(fp);
+		if(a[i] == ' ')
+			a[i] = '_';
+		}//This is for adding '_' in the place of space
+        	strcat(a,b);
+        	printf("%s\n",a);
+        	FILE*fp;
+        	fp=fopen(a,"w");
+        	int n;
+		printf("enter the fields for %s training form\n",c);
+        	do//this will scan the fields for the .xls file 
+        	{
+                	char read[MAX];
+                	scanf(" %[^\n]",read);
+                	fprintf(fp,"%s\t",read);
+                	i++;
+			printf("Do you want add another field(y/n)\n");
+                	scanf(" %c",&field);
+       	 }while(field=='y'||field=='Y');
+		fclose(fp);
+		printf("%s File is created\n\n",a);
 }
 void DeleteVotingRequest()
 {
 	char delete[SIZE];
-	char d[MAX];
+	char d[MAX]; // scaning the file name for deletion
 	char b[MAX] = "_request.xls";
 	char sure;
+	int i;
 	printf("List of xls files present :\n");
-	if(system("ls *.xls")) // Listing the files with .xls extension
+	if(system("ls *request.xls")) // Listing the files with .xls extension
 	{
 		printf("Request form is not created\n");
 		return;
 	}
 	printf("Enter the file name to delete : \n");
-	scanf(" %s",d); // To scan file name
-	char *p = strstr(d,b); // To check "_Request.xls" file is present or not
+	scanf(" %[^\n]",d); // To scan file name
+	for(i=0; d[i]; i++) // To add '_' in place of space
+	{
+		if(d[i] == ' ')
+			d[i] = '_';
+	}
+	
+	char *p = strstr(d,b); // To check "_Request.xls" file is present or not. Otherwise it will add to strcat
 	if(p == NULL)
-		strcat(d,b); // "_request.xls"
+	   strcat(d,b); // "_request.xls"
 	snprintf(delete,sizeof(delete), "rm %s", d); // In delete it will add with  "rm filename.xls"
+	if(!access(d,F_OK)) //access is for checking the file is present or not
+	{
 	printf("Are you sure (y/n)\n");
 	scanf(" %c",&sure);
 	if(sure == 'Y' || sure == 'y')
 	{
 		system(delete); // Deleting particular file
-		printf("%s file is deleted\n",d); // Printing deleted file
+		printf("%s file is deleted\n\n",d); // Printing deleted file
 	}
 	else
 	{
-		printf("%s is not deleted\n", d); 
+		printf("%s is not deleted\n\n", d); 
+	}
+	}
+	else
+	{
+		printf("%s  File does not created\n\n", d);
 	}
 }
 
--- Voting_Request.h
+++ Voting_Request.h
@@ -1,6 +1,9 @@
-#include"main.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
 #define MAX 30
 #define SIZE 200
 void CreateVotingRequest();
 void DeleteVotingRequest();
-void Voting_Request();
+void VotingRequest();
